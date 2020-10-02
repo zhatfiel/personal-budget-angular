9495d1c4d48bf806b5e4ca792a92bc151969876d
zacharyOH90 <zacharyoh90@gmail.com>
2020-10-02 01:04:06 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,34 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+var fs = require('fs');
 
 app.use('/', express.static('public'));
-
-const budget = {
-    myBudget: [
-    {
-        title: 'Eat Out',
-        budget: 25
-    },
-    {
-        title: 'Rent',
-        budget: 650
-    },
-    {
-        title: 'Groceries',
-        budget: 300
-    }
-]
-};
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
-app.get('/budget', (req, res) => {
-    res.json(budget);
+app.get('/budget', function(req, res) {
+    console.log("read file, turned to string, parsed to JSON");
+    var json = JSON.parse(fs.readFileSync('./budgetData.json').toString());
+    res.json(json);
 });
 
 app.listen(port, () => {
-    console.log('Example app listening at http://localhost:3000');
+    console.log('Example app listening at http://localhost:' + port);
 });
