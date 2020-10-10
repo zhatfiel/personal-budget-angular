7c2cb9bfe96b4e42c2462ea87dc49db5c8214935
zacharyOH90 <zacharyoh90@gmail.com>
2020-10-09 20:55:56 -0400

    added breadcrumbs and contact components

--- personal-budget/src/app/about/about.component.ts
+++ personal-budget/src/app/about/about.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'pb-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss', '../homepage/homepage.component.scss']
 })
 export class AboutComponent implements OnInit {
 
